76320f6605baf9063cf5243a08fdf3099d9e2f4b
Fatem J <fatima.aljunaid.257@gmail.com>
2024-06-13 10:37:42 +0300

    view Comments

---
+++ src/components/ViewPost.jsx
@@ -0,0 +1,23 @@
+const ViewPost = ({ post }) => {
+  return (
+    <div>
+      <h3>Published By {post.name}</h3>
+      <h3>{post.title}</h3>
+      <p>{post.caption}</p>
+      <details>
+        <summary>Comments</summary>
+        {post.comments.map((comment) => (
+          <div>
+            <div className="user">
+              <img src="avatar" alt="avatar" />
+              {comment.user}
+            </div>
+            <p>{comment.comment}</p>
+          </div>
+        ))}
+      </details>
+    </div>
+  )
+}
+
+export default ViewPost
