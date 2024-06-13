56e8e8d57c80ab668353bbecd5ba72afded4d1f6
Fatem J <fatima.aljunaid.257@gmail.com>
2024-06-13 14:02:41 +0300

    merging

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Nav from './components/Nav'
 import './App.css'
 import Home from './pages/Home'
 import ShowPlan from './pages/ShowPlan'
+import MyPlans from './pages/MyPlans'
 
 const App = () => {
   return (
     <div>
       <Nav />
+      <ShowPlan/>
       <main>
         <Routes>
           <Route path="/about" element={<About />} />
