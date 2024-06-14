2dec2bad11707fdcecfcf523ab65deaccd17ec17
Fatem J <fatima.aljunaid.257@gmail.com>
2024-06-14 13:52:07 +0300

    little fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = ({ user }) => {
           <h3>Published By {post.user.firstName}</h3>
           <h3>{post.title}</h3>
           <p>{post.caption}</p>
-          <div className="button" onClick={navigate(`/posts/${post._id}`)}>Read More</div>
+          <div className="button" onClick={()=> navigate(`/posts/${post._id}`)}>Read More</div>
         </div>
       ))}
     </div>
