87f6cb4d0da42b2c0b54ec782a73737eb3be84b6
hawraalattar <hanovalattar@gmail.com>
2024-06-18 15:53:28 +0300

    deletePlan added

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -1,6 +1,20 @@
 import { Link } from 'react-router-dom'
+import { DeletePlan } from '../services/MealPlanServices'
 
 const Plan = ({ plan }) => {
+  const handleDeletePlan = (index) => {
+    setCommentToDelete(index)
+  }
+
+  const handleConfirmDeletePlan = () => {
+    DeleteComment(commentToDelete)
+    setCommentToDelete(-1)
+  }
+
+  const handleCancelDeletePlan = () => {
+    setCommentToDelete(-1)
+  }
+
   return (
     <div>
       <h3>{plan.planName}</h3>
