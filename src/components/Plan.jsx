33635fa6012b7282222d099e353574520cca6ae5
Fatem J <fatima.aljunaid.257@gmail.com>
2024-06-18 06:38:53 +0300

    tiny fixes in plan and post files

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 const Plan = ({ plan }) => {
   return (
     <div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,17 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from 'react-router-dom'
 
-const Post = ({post}) =>{
+const Post = ({ post }) => {
   let navigate = useNavigate()
-  return(
+
+  return (
     <div className="feed-post">
-          <h3>Published By {post.user.firstName}</h3>
-          <h3>{post.title}</h3>
-          <p>{post.caption}</p>
-          <div className="button" onClick={()=> navigate(`/posts/${post._id}`)}>Read More</div>
-        </div>
+      <h3>Published By {post.user.firstName}</h3>
+      <h3>{post.title}</h3>
+      <p>{post.caption}</p>
+      <div className="button" onClick={() => navigate(`/posts/${post._id}`)}>
+        Read More
+      </div>
+    </div>
   )
 }
 
